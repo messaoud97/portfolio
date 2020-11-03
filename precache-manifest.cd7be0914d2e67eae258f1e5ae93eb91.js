f439bea4f96ea77489ac090aba473af75dfcde11
messaoud.ahmed <messaoud.ahmed97@gmail.com>
2020-11-03 02:09:53 +0100

    Updates

---
+++ precache-manifest.cd7be0914d2e67eae258f1e5ae93eb91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edb13de03bac4ad40accdf829def4e5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "779ba50830494faf4928",
+    "url": "/portfolio/static/js/2.0adbb516.chunk.js"
+  },
+  {
+    "revision": "46f975db50db0265fd51fd36aa9127c4",
+    "url": "/portfolio/static/js/2.0adbb516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30c3c3a0f7c3064fbac",
+    "url": "/portfolio/static/js/main.986a8fd6.chunk.js"
+  },
+  {
+    "revision": "6985874efa114f4e5e9f",
+    "url": "/portfolio/static/js/runtime-main.0b8dd63a.js"
+  },
+  {
+    "revision": "664bfe49667d2cf998db59f225793d22",
+    "url": "/portfolio/static/media/Enicarthage.664bfe49.png"
+  },
+  {
+    "revision": "84e5840cf45335e771c9f19c1cd967a8",
+    "url": "/portfolio/static/media/isamm.84e5840c.png"
+  },
+  {
+    "revision": "bafa3f7cd120d3d6c5c0f44b4c4810c0",
+    "url": "/portfolio/static/media/k2lis.bafa3f7c.png"
+  },
+  {
+    "revision": "25ed99b69428d75f702edfaa801631f3",
+    "url": "/portfolio/static/media/tt.25ed99b6.png"
+  }
+]);
